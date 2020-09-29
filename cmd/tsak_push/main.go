6df08366fbc836a20a1651366b742c845f1895d6
Vladimir Ulogov <vladimir.ulogov@me.com>
2020-09-28 19:53:16 -0700

    Initial commit

--- cmd/tsak_push/main.go
+++ cmd/tsak_push/main.go
@@ -2,8 +2,9 @@ package main
 
 import (
   "fmt"
+  "github.com/vulogov/TelemetrySAK/internal/server"
 )
 
 func main() {
-  fmt.Println("Hello, world.")
+  main.server_main()
 }
---
+++ internal/server/main.go
@@ -0,0 +1,3 @@
+func server_main() {
+  fmt.Println("Hello server")
+}
