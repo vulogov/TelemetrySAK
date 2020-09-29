be89de8ddfcc6a16f5cf5e54794bb84d57942a70
Vladimir Ulogov <vladimir.ulogov@me.com>
2020-09-28 20:52:28 -0700

    Begin w command line arguments processing

--- internal/server/init.go
+++ internal/server/init.go
@@ -3,9 +3,15 @@ package server
 import (
   "os"
   "github.com/sirupsen/logrus"
+  "flag"
+  "fmt"
 )
 
 var log = logrus.New()
+var port int
+var host string
+var pub string
+
 
 func Init() {
   log.Formatter = new(logrus.TextFormatter)
@@ -16,4 +22,9 @@ func Init() {
 	log.Out = os.Stdout
 
   log.Trace("Server initialization")
+  flag.IntVar(&port, "port", 61001, "Listening port")
+  flag.StringVar(&host, "address", "127.0.0.1", "Listening host")
+  flag.StringVar(&pub, "pub", "tcp://127.0.0.1:61002", "PUB service")
+  flag.Parse()
+  log.Info(fmt.Sprintf("Listening on %[1]s:%[2]d", host, port))
 }
