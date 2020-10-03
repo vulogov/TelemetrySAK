a65c281f4a405caa8cfa39c795c6f467ecada913
Vladimir Ulogov <vladimir.ulogov@me.com>
2020-10-02 22:28:53 -0700

    Non-functional Traces

--- packages/nr.go
+++ packages/nr.go
@@ -70,9 +70,42 @@ func Events(nrikey, url string, _payload []byte) {
   log.Trace(fmt.Sprintf("Body: %v",string(body)))
 }
 
+func Traces(nrikey string, url string, compress bool, _payload []byte) {
+  var payload []byte
+  var b bytes.Buffer
+  if compress {
+    log.Trace("Compression: ON")
+    w := gzip.NewWriter(&b)
+    w.Write([]byte(_payload))
+    w.Close()
+    payload = []byte(b.Bytes())
+  } else {
+    log.Trace("Compression: OFF")
+    payload = []byte(_payload)
+  }
+  req, err := http.NewRequest("POST", url, bytes.NewBuffer(payload))
+  if err != nil {
+    log.Error(fmt.Sprintf("%v",err))
+    return
+  }
+  req.Header.Set("X-Insert-Key", nrikey)
+  req.Header.Set("Content-Type", "application/json")
+  if compress {
+    req.Header.Set("Content-Encoding", "gzip")
+  }
+  client := &http.Client{}
+  resp, err := client.Do(req)
+  defer resp.Body.Close()
+  log.Trace(fmt.Sprintf("Status: %v",resp.Status))
+  log.Trace(fmt.Sprintf("Headers: %v",resp.Header))
+  body, _ := ioutil.ReadAll(resp.Body)
+  log.Trace(fmt.Sprintf("Body: %v",string(body)))
+}
+
 func init() {
   env.Packages["nr"] = map[string]reflect.Value{
     "Metrics":                reflect.ValueOf(Metrics),
     "Events":                 reflect.ValueOf(Events),
+    "Traces":                 reflect.ValueOf(Traces),
   }
 }
